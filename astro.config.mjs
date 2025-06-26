d671ee30b223d0c30b9653c4efb5f1b051ad92ae
Bernard McWeeney <bernardmcweeney@outlook.com>
2025-06-26 21:02:08 +0100

    designed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
-// @ts-check
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://bernardmcweeney.com",
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
   }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
